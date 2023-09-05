108b1f73061dcd9231af339778d8a11c90b5f27f
LahceneDer <l.dergham@esi-sba.dz>
2023-09-06 00:12:55 +0100

    complete ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { BsCurrencyDollar } from 'react-icons/bs';
-import { GoPrimitiveDot } from 'react-icons/go';
-import { IoIosMore } from 'react-icons/io';
-import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoDotFill } from "react-icons/go";
+import { IoIosMore } from "react-icons/io";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 
-import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
-import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
-import product9 from '../data/product9.jpg';
+import { Stacked, Pie, Button, LineChart } from "../components";
+import {
+  earningData,
+  medicalproBranding,
+  recentTransactions,
+  weeklyStats,
+  dropdownData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+import product9 from "../data/product9.jpg";
+import SparkLine from "../components/Charts/SparkLine";
 
 const Ecommerce = () => {
   return (
@@ -20,13 +29,22 @@ const Ecommerce = () => {
               <p className="text-2xl">$63,448.78</p>
             </div>
           </div>
-          <div className='mt-6'>
-          <Button color='white' bgColor='blue' borderRadius='10px' size='sm' text='Download' />
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor="blue"
+              borderRadius="10px"
+              size="sm"
+              text="Download"
+            />
           </div>
         </div>
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
           {earningData.map((item) => (
-            <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
+            <div
+              key={item.title}
+              className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
+            >
               <button
                 type="button"
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
@@ -46,8 +64,61 @@ const Ecommerce = () => {
         </div>
       </div>
 
-    </div>
-  )
-}
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoDotFill />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoDotFill />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-10 flex gap-10 flex-wrap justify-center">
+        <div className="border-r-1 border-color m-4 pr-10">
+          <div>
+            <p>
+              <span className="text-3xl font-semibold">$93,438</span>
+              <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                23%
+              </span>
+            </p>
+            <p className="text-gray-500 mt-1">Budget</p>
+          </div>
+          <div className="mt-8">
+                <p className="text-3xl font-semibold">$48,487</p>
 
-export default Ecommerce
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              <div className="mt-5">
+                <SparkLine currentColor='green' id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color="green" />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+        </div>
+        <div>
+              <Stacked currentMode="green" width="320px" height="360px" />
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ecommerce;
